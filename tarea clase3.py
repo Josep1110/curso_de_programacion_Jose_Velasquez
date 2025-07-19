292eee6a1ea343616ae812375adbef8982660a1d
Josep1110 <joseavelasquezm2a@gmail.com>
2025-07-19 13:10:16 -0400

    clase3

---
+++ tarea clase3.py
@@ -0,0 +1,34 @@
+#Examen practico 
+
+#1
+ 
+edad = 16 
+print(f"Tengo {edad} años")
+
+#5
+
+num = int(input("Ingrese un numero")) 
+
+if num % 2 == 0:
+    print("El numero es par")
+else:
+    print("El numero es impar")
+
+#8
+
+variable = int(input("Ingrese un numero"))
+
+if variable > 10 and variable < 20:
+    print("El valor esta en el rango")
+else:
+    print("El valor no esta en el rango")
+    
+#9
+
+x = 10
+y = 10 
+
+if x == y:
+    print("x e y son iguales")
+else:
+    print("x e y son diferentes")
