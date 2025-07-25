e1815f39bbbbfb58410d2305b2872a76d3b3fb17
Josep1110 <joseavelasquezm2a@gmail.com>
2025-07-24 20:47:42 -0400

    Add files via upload

---
+++ Modulo_1/python/ejercicios bucles.py
@@ -0,0 +1,10 @@
+#bucle for 
+for numero in range(10):
+    print(f"numero actual: {numero + 1}")
+
+#bucle while
+contador = 0
+while contador < 11:
+    print(f"Contador: {contador}")
+    contador += 1
+
