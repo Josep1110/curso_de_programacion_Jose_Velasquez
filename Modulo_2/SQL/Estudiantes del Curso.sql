6faf7d9d2c6797c590cec537f78e5122406be54d
Josep1110 <joseavelasquezm2a@gmail.com>
2025-08-08 14:02:04 -0400

    Add files via upload

---
+++ Modulo_2/SQL/Estudiantes del Curso.sql
@@ -0,0 +1,52 @@
+CREATE TABLE Students (
+    student_id INT PRIMARY KEY, 
+    first_name VARCHAR(100), 
+    last_name VARCHAR(100), 
+    age INT, 
+    ID INT
+);
+
+INSERT INTO Students (student_id, first_name, last_name, age, ID)
+VALUES (1,'Jose', 'Velasquez', 16, "32698769");
+
+INSERT INTO Students (student_id, first_name, last_name, age, ID)
+VALUES (2,'Juan', 'Polania', 17, "32636138");
+
+INSERT INTO Students (student_id, first_name, last_name, age, ID)
+VALUES (3,'Roberto', 'Borges', 25, "16186354");
+
+INSERT INTO Students (student_id, first_name, last_name, age, ID)
+VALUES (4,'Jinmy', 'Contreras', 20, "29767741");
+
+INSERT INTO Students (student_id, first_name, last_name, age, ID)
+VALUES (5,'Maria', 'Urdaneta', 22 , "30604870");
+
+INSERT INTO Students (student_id, first_name, last_name, age, ID)
+VALUES (6,'Daniel', 'Soto', 20, "30820000");
+
+INSERT INTO Students (student_id, first_name, last_name, age, ID)
+VALUES (7,'Daviannys', 'Villalobos', 19, "31279773");
+
+INSERT INTO Students (student_id, first_name, last_name, age, ID)
+VALUES (8,'Humberto', 'Alvarez', 20, "31913070");
+
+INSERT INTO Students (student_id, first_name, last_name, age, ID)
+VALUES (9,'German', 'Suarez', 19, "32056687");
+
+INSERT INTO Students (student_id, first_name, last_name, age, ID)
+VALUES (10,'Julio', 'Pacheco', 19, "32410084");
+
+INSERT INTO Students (student_id, first_name, last_name, age, ID)
+VALUES (11,'Gabriel', 'Chacin', 19, "33319114");
+
+INSERT INTO Students (student_id, first_name, last_name, age, ID)
+VALUES (12,'Robert', 'Boscan', 17, "33607751");
+
+INSERT INTO Students (student_id, first_name, last_name, age, ID)
+VALUES (13,'Carlos', 'Moreno', 19, "33893333");
+
+INSERT INTO Students (student_id, first_name, last_name, age, ID)
+VALUES (14,'Jimn', 'Chacin', 19, "33966984");
+
+INSERT INTO Students (student_id, first_name, last_name, age, ID)
+VALUES (15,'Juan', 'Arrieta', 19, "34544638");
