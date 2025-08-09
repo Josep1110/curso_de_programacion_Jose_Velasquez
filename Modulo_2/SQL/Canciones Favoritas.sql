2d0406ba500c96bd8bb0e03b1373dc78d4be2a65
Josep1110 <joseavelasquezm2a@gmail.com>
2025-08-09 13:20:29 -0400

    Add files via upload

---
+++ Modulo_2/SQL/Canciones Favoritas.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Canciones_Favoritas (
+    Nombre VARCHAR(100),
+    Artista_principal VARCHAR(100),
+    Features VARCHAR(100),
+    Album VARCHAR(100),
+    Año_de_publicacion INT,
+    productores VARCHAR(100)
+); 
