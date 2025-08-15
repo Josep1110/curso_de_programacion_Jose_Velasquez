e05cf510fb7e049ba8d7f5858870a2d9c807747b
Josep1110 <joseavelasquezm2a@gmail.com>
2025-08-15 12:39:10 -0400

    Add files via upload

---
+++ Modulo_3/PHP/declaracion de variable.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$nombre = "Jose Velasquez";
+
+$edad = 16;
+
+$descripcion = "mido 1.65 metros, soy de Maracaibo, Venezuela y recien me gradue del bachillerato";
+
+
+echo "Mis Datos:\n";
+var_dump($nombre);
+var_dump($edad);
+var_dump($descripcion);
+echo "\n----------------------------------------\n\n";
+
+
+?>
