2355f516e80a21ea6eb9cd51205924f01629cb63
Josep1110 <joseavelasquezm2a@gmail.com>
2025-08-15 18:29:57 -0400

    Update Canciones Favoritas.sql

--- Modulo_2/SQL/Canciones Favoritas.sql
+++ Modulo_2/SQL/Canciones Favoritas.sql
@@ -16,7 +16,7 @@ INSERT INTO Canciones_Favoritas (Nombre, Artista_principal, Features, Album, Añ
 ('All Caps', 'Madvillain', 'MF DOOM, Madlib', 'Madvillainy', 2004, 'Madlib'),
 ('White Dress', 'Kanye West', 'None', 'The Man With The Iron Fists (Original Motion Picture Soundtrack)', 2012, 'RZA, Boi-1da, Hudson Mohawke, Kanye West'),
 ('Homecoming', 'Kanye West', 'Chris Martin', 'Graduation', 2007, 'Kanye West, Warryn Campbell'),
-('Saint Pablo', 'Kanye West', 'None', 'The Life of Pablo', 2016, 'Kanye West, Mike Dean, G-Funk'),
+('Saint Pablo', 'Kanye West', 'Sampha', 'The Life of Pablo', 2016, 'Kanye West, Mike Dean, G-Funk'),
 ('Nokia', 'Drake', 'None', 'Some Sexy Songs 4 U', 2025, 'Elkan'),
 ('Gorgeous', 'Kanye West', 'Kid Cudi, Raekwon', 'My Beautiful Dark Twisted Fantasy', 2010, 'Kanye West, Mike Dean'),
 ('Alright', 'Kendrick Lamar', 'Pharrell Williams', 'To Pimp A Butterfly', 2015, 'Pharrell Williams, Sounwave'),
@@ -24,3 +24,4 @@ INSERT INTO Canciones_Favoritas (Nombre, Artista_principal, Features, Album, Añ
 ('Luther (With SZA)', 'Kendrick Lamar', 'SZA', 'Black Panther: The Album', 2018, 'Sounwave, Terrace Martin, Kendrick Lamar'),
 ('Passionfruit', 'Drake', 'None', 'More Life', 2017, 'Nana Rogues'),
 ('Feel It', 'D4VD', 'None', 'Petals To Thorns', 2023, 'D4VD');
+
