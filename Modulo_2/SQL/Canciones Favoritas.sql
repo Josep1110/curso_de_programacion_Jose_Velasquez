6f7f3d7ff6d7a6638e8cb93287edca32a74cc695
Josep1110 <joseavelasquezm2a@gmail.com>
2025-08-15 17:53:52 -0400

    Update Canciones Favoritas.sql

--- Modulo_2/SQL/Canciones Favoritas.sql
+++ Modulo_2/SQL/Canciones Favoritas.sql
@@ -4,5 +4,23 @@ CREATE TABLE Canciones_Favoritas (
     Features VARCHAR(100),
     Album VARCHAR(100),
     Año_de_publicacion INT,
-    productores VARCHAR(100)
+    Productores VARCHAR(100)
+
 ); 
+
+INSERT INTO Canciones_Favoritas (Nombre, Artista_principal, Features, Album, Año_de_publicacion, Productores) VALUES
+('Devil In A New Dress', 'Kanye West', 'Rick Ross', 'My Beautiful Dark Twisted Fantasy', 2010, 'Kanye West, Bink!'),
+('Ghost Town', 'Kanye West', 'PARTYNEXTDOOR', 'ye', 2018, 'Kanye West, PARTYNEXTDOOR, Mike Dean'),
+('30 Hours', 'Kanye West', 'None', 'The Life of Pablo', 2016, 'Kanye West, Mike Dean'),
+('No More Parties In La', 'Kanye West', 'Kendrick Lamar', 'The Life of Pablo', 2016, 'Kanye West, Madlib'),
+('All Caps', 'Madvillain', 'MF DOOM, Madlib', 'Madvillainy', 2004, 'Madlib'),
+('White Dress', 'Kanye West', 'None', 'The Man With The Iron Fists (Original Motion Picture Soundtrack)', 2012, 'RZA, Boi-1da, Hudson Mohawke, Kanye West'),
+('Homecoming', 'Kanye West', 'Chris Martin', 'Graduation', 2007, 'Kanye West, Warryn Campbell'),
+('Saint Pablo', 'Kanye West', 'None', 'The Life of Pablo', 2016, 'Kanye West, Mike Dean, G-Funk'),
+('Nokia', 'Drake', 'None', 'Some Sexy Songs 4 U', 2025, 'Elkan'),
+('Gorgeous', 'Kanye West', 'Kid Cudi, Raekwon', 'My Beautiful Dark Twisted Fantasy', 2010, 'Kanye West, Mike Dean'),
+('Alright', 'Kendrick Lamar', 'Pharrell Williams', 'To Pimp A Butterfly', 2015, 'Pharrell Williams, Sounwave'),
+('20 Min', 'Lil Uzi Vert', 'None', 'Luv Is Rage 2', 2017, 'Honorable C.N.O.T.E., Metro Boomin, Don Cannon'),
+('Luther (With SZA)', 'Kendrick Lamar', 'SZA', 'Black Panther: The Album', 2018, 'Sounwave, Terrace Martin, Kendrick Lamar'),
+('Passionfruit', 'Drake', 'None', 'More Life', 2017, 'Nana Rogues'),
+('Feel It', 'D4VD', 'None', 'Petals To Thorns', 2023, 'D4VD');
